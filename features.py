e526bb79fa6e41737d4f43a312599a777ed402de
= <=>
2014-11-19 23:23:36 -0500

    forgot to commit feature.py earlier

--- features.py
+++ features.py
@@ -7,27 +7,31 @@ import numpy as nump
 import cv2
 import test_image
 import dbsetup
+import time
 
-f = open('features','w')
-temp2 = list()
-coordinates = list()
-img_path = 'path-image-110.tif'
-im = cv2.imread(img_path)
-imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-contr_list = []
-means_list = []
+
 def parse_data(path):
-	initializeBaseDB()
+	dbsetup.initializeBaseDB()
+	image_path = "/data_595_input";
         pattern = '*.txt'
-	f.write('Area\tPerimeter\tEqui-diameter\tisContour\tConvex Area\tSolidity\tEllipse\tMajor Axis\tMinor Axis\tEccentricity\tMean\n')
         for root, dirs, files in os.walk(path):
                 for filename in fnmatch.filter(files,pattern):
                         print os.path.join(root,filename)
+			nuclie_id = 0
+			t = time.time()	
                         with open(os.path.join(root,filename),'r') as text:
                                 next(text)
+				for file in os.listdir(image_path):
+					if file.startswith(filename[0:filename.index('.seg')]) and file.endswith(".tif"):
+						im = cv2.imread(os.path.join(image_path,file))
+						imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+						break
                                 for line in text:
 					datalist = []
-					datalist.append(filename)
+					datalist.append(filename[0:filename.index('.seg')])
+					nuclie_id += 1
+					datalist.append(nuclie_id)
+		
 					line = line.strip()
 					boundarys = line.split('\t')[51].strip()
                                         boundary_vals = boundarys.split(';')
@@ -38,16 +42,19 @@ def parse_data(path):
 					contr = nump.array(boundary_vals)
 					
 					#area
-					area = cv2.contourArea(contr)
+					area = float("{0:.2f}".format(cv2.contourArea(contr)))
 					datalist.append(area)
-					#roundness
-					roundness = (4*3.14*area)/(perimeter*perimeter)
+					
 					#perimeter
-					perimeter = cv2.arcLength(contr,True)
+					perimeter = float("{0:.2f}".format(cv2.arcLength(contr,True)))
 					datalist.append(perimeter)
+					
+					#roundness
+                                        roundness = float("{0:.2f}".format((4*3.14*area)/(perimeter*perimeter)))
 					datalist.append(roundness)
-    					# equivalent diameter
-    					equi_diameter = nump.sqrt(4*area/nump.pi)
+					
+					#equivalent diameter
+    					equi_diameter = float("{0:.2f}".format(nump.sqrt(4*area/nump.pi)))
 					datalist.append(equi_diameter)
 
     					### CONVEX HULL ###
@@ -55,10 +62,10 @@ def parse_data(path):
     					convex_hull = cv2.convexHull(contr)
 
     					# convex hull area
-    					convex_area = cv2.contourArea(convex_hull)
+    					convex_area = float("{0:.2f}".format(cv2.contourArea(convex_hull)))
 					datalist.append(convex_area)
     					# solidity = contour area / convex hull area
-    					solidity = area/float(convex_area)
+    					solidity = float("{0:.2f}".format(area/float(convex_area)))
 					datalist.append(solidity)
     					### ELLIPSE  ###
 
@@ -68,33 +75,33 @@ def parse_data(path):
     					(center,axes,orientation) = ellipse
 
     					# length of MAJOR and minor axis
-    					majoraxis_length = max(axes)
-    					minoraxis_length = min(axes)
+    					majoraxis_length = float("{0:.2f}".format(max(axes)))
+    					minoraxis_length = float("{0:.2f}".format(min(axes)))
 					datalist.append(majoraxis_length)
 					datalist.append(minoraxis_length)
     					# eccentricity = sqrt( 1 - (ma/MA)^2) --- ma= minor axis --- MA= major axis
-    					eccentricity = nump.sqrt(1-(minoraxis_length/majoraxis_length)**2)
+    					eccentricity = float("{0:.2f}".format(nump.sqrt(1-(minoraxis_length/majoraxis_length)**2)))
 					datalist.append(eccentricity)
 
-					datalist.append(boundarys)
+					datalist.append(str(boundarys))
 					# Find Mean Pixel Intensity
 					mask = nump.zeros(imgray.shape,nump.uint8)
 					cv2.drawContours(mask,[contr],0,255,-1)
 					mean = cv2.mean(im,mask = mask)
-					means_list.append(list(mean[0:3]))
-					contr_list.append(contr)
-					datalist.append(means_list)
-					datalist.append(None)
-					datalist.append(None)
-					insertData(datalist,False)
-    					f.write(str(area) + '\t' + str(perimeter)+ '\t' + str(roundness))
-    					f.write('\t' + str(equi_diameter) + '\t' + str(isContour) + '\t' + str(convex_area))
-    					f.write('\t' + str(solidity) + '\t' + str(ellipse) + '\t' + str(majoraxis_length) + '\t' + str(minoraxis_length) + '\t' + str(eccentricity) + '\n')
-					f.write('\t Mean = ' + str(mean) + '\n') 
-
-	f.close
-	test_image.image_clustering(means_list, contr_list, img_path)
-
+					datalist.append(str(mean[0:3]))
+					(min_val,max_val,min_loc,max_loc) = cv2.minMaxLoc(imgray,mask = mask)
+					min_val = float("{0:.2f}".format(min_val))
+					max_val = float("{0:.2f}".format(max_val))
+					datalist.extend([max_val,min_val])
+					s = time.time()
+					dbsetup.insertData(datalist,False)
+					print "time for nuclie:%s - %d is %d"%(datalist[0],datalist[1],time.time()-s)
+			print "Parsing done for %s ! Time taken %d "%(filename,float((time.time()-t)/60))				
+	#test_image.image_clustering(means_list, contr_list, img_path)
+	dbsetup.closeConnBaseDB()
 if __name__ == '__main__':
+	p = time.time()
         path = sys.argv[1]
         parse_data(path)
+	print 'END TIME:',time.time()-p
+
